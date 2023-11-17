b4573b38ac53a5db052d38da629b28ab0be51699
Rafael Carvalho <rafaelvfcarvalho@gmail.com>
2023-11-17 11:03:36 -0300

    add example

---
+++ jsonexample.ts
@@ -0,0 +1,31 @@
+const table = {
+  module: 'commercial',
+  name: 'persons_contacts',
+  entity: 'PersonContact',
+  entityCamel: 'personContact',
+  entityPlural: 'PersonsContacts',
+  entityPluralCamel: 'personsContacts',
+  cache: 'person-contact-get-${id}',
+  cacheList: 'persons-contacts-list',
+  columns: [
+    { name: 'name', type: 'int', jsType: 'number', null: false },
+    {
+      name: 'type',
+      type: `enum('location', 'sale')`,
+      jsType: 'number',
+      null: true,
+    },
+  ],
+  relations: [
+    {
+      name: 'persons',
+      column: 'person_id',
+      relatedColumn: 'id',
+      entity: 'Person',
+      type: 'ManyToOne',
+      columnNameInEntity: 'person',
+    },
+  ],
+};
+
+export default table;
